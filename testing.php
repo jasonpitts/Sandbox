64bf46d7157db5296bcb7522297a62b7920491a1
Jason Pitts <jasonpitts@fastmail.fm>
2012-05-02 22:06:23 -0700

    Awesome

--- testing.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "for some reason it is not letting me upload other files that are in the project and I am not sure why. But I suspect it is because they were preexisting files before I did my initial fetch.";
-
-?>
